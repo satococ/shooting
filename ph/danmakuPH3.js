f87bcf73ad42c094f668e1c5cc9819557d25d3fc
yt18304019 <44827902+yt18304019@users.noreply.github.com>
2019-09-25 12:48:03 +0900

    BI1作成

--- ph/danmakuPH3.js
+++ ph/danmakuPH3.js
@@ -34,8 +34,8 @@ function updateTimetText(){
         //javascriptでは文字列数列を連結すると文字列になる
         //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡してやる。
         m = ('0' + m).slice(-2);
-        s = ('0' + s).slice(-2);
-        ms = ('0' + ms).slice(-3);
+        s = ('0' + (29-s)).slice(-2);
+        ms = ('0' + (999-ms)).slice(-3);
 
         //HTMLのid　timer部分に表示させる　
         timer.textContent ='Time ' + m + ':' + s + ':' + ms;
